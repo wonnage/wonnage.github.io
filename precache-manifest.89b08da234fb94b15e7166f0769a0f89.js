1cad5f55fa43bb6586e0948efe06bbcc0dfd41df
Victor Lin <victorhlin@gmail.com>
2020-02-18 19:55:35 -0800

    Updates

---
+++ precache-manifest.89b08da234fb94b15e7166f0769a0f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afaac9c5df5f28bd5792fd70ecffd53b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "465ebe1476b7e2b2ceaa",
+    "url": "/static/css/main.dcfeae92.chunk.css"
+  },
+  {
+    "revision": "992736d798ac4fbf04b8",
+    "url": "/static/js/2.49f84f25.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.49f84f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465ebe1476b7e2b2ceaa",
+    "url": "/static/js/main.5573eba2.chunk.js"
+  },
+  {
+    "revision": "71fb3ccb683238ba1978",
+    "url": "/static/js/runtime-main.84ac0255.js"
+  },
+  {
+    "revision": "9937c7d81d6d2bdb5a6733fd7a518b4d",
+    "url": "/static/media/logo.9937c7d8.jpeg"
+  }
+]);
